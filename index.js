d13df989ce4daa6564220d466f88bf49dbbceb38
uiolee <22849383+uiolee@users.noreply.github.com>
2023-07-15 13:59:03 +0800

    fix: run when deploy

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ hexo.config.absolute = Object.assign({
 
 const absolute = hexo.config.absolute;
 
-if (absolute.enable && hexo.env.cmd.startsWith('g')) {
+if (absolute.enable && (hexo.env.cmd.startsWith('g') || hexo.env.cmd.startsWith('d'))) {
   hexo.extend.filter.register('after_render:html', filterFn, absolute.priority);
 }
