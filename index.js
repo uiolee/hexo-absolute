7f974a43dddfd28d53eb5a28afa325e20c6dc055
uiolee <22849383+uiolee@users.noreply.github.com>
2023-08-29 20:09:55 +0800

    style: apply eslint

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 /* global hexo */
 
-'use strict';
+"use strict";
 
-const filterFn = require('./lib/filter');
+const filterFn = require("./lib/filter");
 
-hexo.config.absolute = Object.assign({
-  enable: true,
-  tagName: ['a', 'link', 'img', 'script'],
-  attribute: ['href', 'src'],
-  priority: 20
-}, hexo.config.absolute);
+hexo.config.absolute = Object.assign(
+  {
+    enable: true,
+    tagName: ["a", "link", "img", "script"],
+    attribute: ["href", "src"],
+    priority: 20,
+  },
+  hexo.config.absolute,
+);
 
 const absolute = hexo.config.absolute;
 
-if (absolute.enable && (hexo.env.cmd.startsWith('g') || hexo.env.cmd.startsWith('d'))) {
-  hexo.extend.filter.register('after_render:html', filterFn, absolute.priority);
+if (
+  absolute.enable &&
+  (hexo.env.cmd.startsWith("g") || hexo.env.cmd.startsWith("d"))
+) {
+  hexo.extend.filter.register("after_render:html", filterFn, absolute.priority);
 }
